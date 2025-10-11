562a928b4d850e9a8710c9306d13f1281ebc9ef7
simanuki mamoru <mamoru.simanuki74@gmail.com>
2025-10-11 17:37:46 +0900

    2025/10/11 バリデーション設定時の日本語表示になるように設定追加

---
+++ src/resources/lang/ja/en/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed'   => 'ログイン情報が登録されていません',
+    'password' => 'ログイン情報が登録されていません',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+];
---
+++ src/resources/lang/ja/en/validation.php
@@ -0,0 +1,16 @@
+<?php
+return [
+  'custom' => [
+    'email' => [
+      'required' => 'メールアドレスを入力してください',
+      'email'    => 'メールアドレスが正しくありません。',
+    ],
+    'password' => [
+      'required' => 'パスワードを入力してください',
+    ],
+  ],
+  'attributes' => [
+    'email' => 'メールアドレス',
+    'password' => 'パスワード',
+  ],
+];
